e70b5e555fcfc8f968e6cd7bd09a8ea37a703a7d
Alexander Ormseth <a_ormseth@hotmail.com>
2023-11-28 14:58:13 +0100

    feat: added tests to FormTextArea and FormTextField components 🧪

---
+++ CodeShare.Client/src/app/components/common/FormTextField/FormTextField.test.tsx
@@ -0,0 +1,58 @@
+import { render, screen } from "@testing-library/react";
+import FormTextField from "./FormTextField";
+
+describe("FormTextField", () => {
+  const mockRegister = {
+    onChange: vi.fn(),
+    onBlur: vi.fn(),
+    name: "mock",
+    ref: vi.fn()
+  };
+
+  it("renders the label correctly", () => {
+    render(<FormTextField label="Test Label" register={mockRegister} />);
+
+    const label = screen.getByRole("textbox", { name: /Test Label/i });
+    expect(label).toBeInTheDocument();
+  });
+
+  it("renders the required field correctly", () => {
+    render(
+      <FormTextField
+        label="Test Label"
+        register={mockRegister}
+        required={true}
+      />
+    );
+
+    const requiredField = screen.getByRole("textbox", { name: /required/i });
+    expect(requiredField).toBeInTheDocument();
+  });
+
+  it("renders the optional field correctly", () => {
+    render(
+      <FormTextField
+        label="Test Label"
+        register={mockRegister}
+        required={false}
+      />
+    );
+
+    const optionalField = screen.getByRole("textbox", { name: /optional/i });
+    expect(optionalField).toBeInTheDocument();
+  });
+
+  it("renders the error message when error prop is provided", () => {
+    render(
+      <FormTextField
+        label="Test Label"
+        register={mockRegister}
+        error={{ type: "test", message: "Test error" }}
+      />
+    );
+
+    const errorMessage = screen.getByText("Test error");
+    expect(errorMessage).toBeInTheDocument();
+    expect(errorMessage.textContent).toBe("Test error");
+  });
+});
