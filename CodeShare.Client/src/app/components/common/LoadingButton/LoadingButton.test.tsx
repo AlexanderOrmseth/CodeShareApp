7cc0626cf9d9b302a23f9d7fb956c50df298c265
Alexander Ormseth <a_ormseth@hotmail.com>
2023-11-29 10:19:47 +0100

    test: added describe to LoadingButton test

--- CodeShare.Client/src/app/components/common/LoadingButton/LoadingButton.test.tsx
+++ CodeShare.Client/src/app/components/common/LoadingButton/LoadingButton.test.tsx
@@ -1,66 +1,67 @@
 import { render, screen, fireEvent } from "@testing-library/react";
-
 import LoadingButton from "./LoadingButton";
 
-it("renders LoadingButton correctly", () => {
-  render(
-    <LoadingButton
-      loading={false}
-      buttonText="Click me"
-      loadingText="Loading..."
-    />
-  );
+describe("LoadingButton", () => {
+  it("renders LoadingButton correctly", () => {
+    render(
+      <LoadingButton
+        loading={false}
+        buttonText="Click me"
+        loadingText="Loading..."
+      />
+    );
 
-  const button = screen.getByRole("button");
-  expect(button).toBeInTheDocument();
-});
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+  });
 
-it("shows loader and loading text when loading is true", () => {
-  render(
-    <LoadingButton
-      loading={true}
-      buttonText="Click me"
-      loadingText="Loading..."
-    />
-  );
+  it("shows loader and loading text when loading is true", () => {
+    render(
+      <LoadingButton
+        loading={true}
+        buttonText="Click me"
+        loadingText="Loading..."
+      />
+    );
 
-  const button = screen.getByRole("button");
-  expect(button).toHaveTextContent("Loading...");
-});
+    const button = screen.getByRole("button");
+    expect(button).toHaveTextContent("Loading...");
+  });
 
-it("fires onClick handler when clicked and not loading", () => {
-  const onClickMock = vi.fn();
+  it("fires onClick handler when clicked and not loading", () => {
+    const onClickMock = vi.fn();
 
-  render(
-    <LoadingButton
-      loading={false}
-      buttonText="Click me"
-      loadingText="Loading..."
-      onClick={onClickMock}
-    />
-  );
+    render(
+      <LoadingButton
+        loading={false}
+        buttonText="Click me"
+        loadingText="Loading..."
+        onClick={onClickMock}
+      />
+    );
 
-  const button = screen.getByRole("button");
-  fireEvent.click(button);
+    const button = screen.getByRole("button");
+    fireEvent.click(button);
 
-  expect(onClickMock).toHaveBeenCalled();
-});
+    expect(onClickMock).toHaveBeenCalled();
+  });
 
-it("does not fire onClick handler when clicked and loading", () => {
-  const onClickMock = vi.fn();
+  it("does not fire onClick handler when clicked and loading", () => {
+    const onClickMock = vi.fn();
 
-  render(
-    <LoadingButton
-      loading={true}
-      buttonText="Click me"
-      loadingText="Loading..."
-      onClick={onClickMock}
-    />
-  );
+    render(
+      <LoadingButton
+        loading={true}
+        buttonText="Click me"
+        loadingText="Loading..."
+        onClick={onClickMock}
+      />
+    );
 
-  const button = screen.getByRole("button");
-  expect(button).toHaveTextContent("Loading...");
+    const button = screen.getByRole("button");
+    expect(button).toHaveTextContent("Loading...");
 
-  fireEvent.click(button);
-  expect(onClickMock).not.toHaveBeenCalled();
+    fireEvent.click(button);
+    expect(onClickMock).not.toHaveBeenCalled();
+  });
 });
